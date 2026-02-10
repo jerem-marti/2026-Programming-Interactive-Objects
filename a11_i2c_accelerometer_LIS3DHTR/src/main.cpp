f196651916323508f01eed3b165738e7c8f05fdd
Andreas Gysin <andreas@ertdfgcvb.ch>
2026-02-10 12:51:44 +0100

    Update main.cpp

--- a11_i2c_accelerometer_LIS3DHTR/src/main.cpp
+++ a11_i2c_accelerometer_LIS3DHTR/src/main.cpp
@@ -11,18 +11,18 @@
 #include "LIS3DHTR.h"
 #include <Wire.h>
 LIS3DHTR<TwoWire> LIS;
-#define WIRE Wire
 
+#define WIRE Wire
 #define I2C_SDA 23
 #define I2C_SCL 2
 
 void setup() {
 	Wire.begin(I2C_SDA, I2C_SCL);
-	Serial.begin(115200);
-	while (!Serial)
-	{
-	};
-	LIS.begin(WIRE, 0x19); //IIC init
+	Serial.begin(9600);
+
+	while (!Serial) {};
+
+	LIS.begin(Wire, LIS3DHTR_ADDRESS_UPDATED); //IIC init
 	delay(100);
 	LIS.setOutputDataRate(LIS3DHTR_DATARATE_50HZ);
 	LIS.setHighSolution(true); //High solution enable
@@ -31,12 +31,11 @@ void setup() {
 void loop() {
 	if (!LIS) {
 		Serial.println("LIS3DHTR didn't connect.");
-		while (1);
 		return;
 	}
 	//3 axis
-		Serial.print("x:"); Serial.print(LIS.getAccelerationX()); Serial.print("  ");
-		Serial.print("y:"); Serial.print(LIS.getAccelerationY()); Serial.print("  ");
-		Serial.print("z:"); Serial.println(LIS.getAccelerationZ());
+	Serial.print("x:"); Serial.println(LIS.getAccelerationX());
+	Serial.print("y:"); Serial.println(LIS.getAccelerationY());
+	Serial.print("z:"); Serial.println(LIS.getAccelerationZ());
 	delay(10);
 }
